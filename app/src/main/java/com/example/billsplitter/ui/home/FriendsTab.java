62f750f3724b8137c6f4bb75dc87c01e92761786
Akash Bharti <ak420492@dal.ca>
2019-12-02 23:05:16 -0400

    compilation error fix

--- app/src/main/java/com/example/billsplitter/ui/home/FriendsTab.java
+++ app/src/main/java/com/example/billsplitter/ui/home/FriendsTab.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
